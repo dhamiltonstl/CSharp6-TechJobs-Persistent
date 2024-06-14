ea4990f7934748bb0149c100bfc8927b6ea5acf6
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-06-14 13:43:26 -0500

    set up MySQL

---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobs6Persistent.Models;
+
+namespace TechJobs6Persistent.ViewModels;
+{
+   public class AddEmployerViewModel
+   {
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public string? Location { get; set; }
+   }
+}
+
